f167fad702a9e8f2c31b5944140d64adf97e41a0
xiaoxuan <xiaoxuan@qq.com>
2021-08-19 11:39:08 +0800

    定义应用图标组件（AppIcon）

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
   <AppLogo size="64" color="#6435c9" />
+  <AppIcon size="64" color="#6435c9" name="comment" />
 </template>
 
 <script>
 import AppLogo from '@/app/components/app-logo.vue';
+import AppIcon from '@/app/components/app-icon.vue';
 
 export default {
   components: {
     AppLogo,
+    AppIcon,
   },
 };
 </script>
