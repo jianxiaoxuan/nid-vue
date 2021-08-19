33ed18c5a7cf969b0a06f7f59509eefe5fb4ef07
xiaoxuan <xiaoxuan@qq.com>
2021-08-19 15:36:14 +0800

    定义本地存储Store插件（localStorageStorePlugin）

---
+++ src/app/app.store.plugin.ts
@@ -0,0 +1,16 @@
+import { RootState } from './app.store';
+import { setStorage } from './app.service';
+import { Plugin } from 'vuex';
+
+/**
+ * 本地存储
+ */
+export const localStorageStorePlugin: Plugin<RootState> = store => {
+  store.subscribe(mutation => {
+    switch (mutation.type) {
+      case 'layout/setTheme':
+        setStorage('theme', mutation.payload);
+        break;
+    }
+  });
+};
