ff0466b02d4e87ca51535b1ef522ff951206b1d2
xiaoxuan <xiaoxuan@qq.com>
2021-08-19 17:15:50 +0800

    定义用户登录Store模块（authLoginStoreModule）

---
+++ src/auth/auth.store.ts
@@ -0,0 +1,44 @@
+import { Module } from 'vuex';
+import { RootState } from '@/app/app.store';
+import {
+  authLoginStoreModule,
+  AuthLoginStoreState,
+} from './login/auth-login.store';
+
+export interface AuthStoreState {
+  login: AuthLoginStoreState;
+}
+
+export const authStoreModele: Module<AuthStoreState, RootState> = {
+  /**
+   * 命名空间
+   */
+  namespaced: true,
+
+  /**
+   * 数据
+   */
+  state: {} as AuthStoreState,
+
+  /**
+   * 获取器
+   */
+  getters: {},
+
+  /**
+   * 修改器
+   */
+  mutations: {},
+
+  /**
+   * 动作
+   */
+  actions: {},
+
+  /**
+   * 模块
+   */
+  modules: {
+    login: authLoginStoreModule,
+  },
+};
---
+++ src/auth/login/auth-login.store.ts
@@ -0,0 +1,76 @@
+import { Module } from 'vuex';
+import { RootState } from '@/app/app.store';
+import { apiHttpClient } from '@/app/app.service';
+
+export interface LoginResponseData {
+  id: number;
+  name: string;
+  token: string;
+}
+
+export interface AuthLoginStoreState {
+  loading: boolean;
+  loginResponseData: LoginResponseData | null;
+}
+
+export const authLoginStoreModule: Module<AuthLoginStoreState, RootState> = {
+  /**
+   * 命名空间
+   */
+  namespaced: true,
+
+  /**
+   * 数据
+   */
+  state: {
+    loading: false,
+    loginResponseData: null,
+  } as AuthLoginStoreState,
+
+  /**
+   * 获取器
+   */
+  getters: {
+    loading(state) {
+      return state.loading;
+    },
+
+    responseData(state) {
+      return state.loginResponseData;
+    },
+  },
+
+  /**
+   * 修改器
+   */
+  mutations: {
+    setLoading(state, data) {
+      state.loading = data;
+    },
+
+    setLoginResponseData(state, data) {
+      state.loginResponseData = data;
+    },
+  },
+
+  /**
+   * 动作
+   */
+  actions: {
+    async login({ commit }) {
+      commit('setLoading', true);
+
+      try {
+        const response = await apiHttpClient.post('/login');
+        commit('setLoginResponseData', response.data);
+        commit('setLoading', false);
+
+        return response;
+      } catch (error) {
+        commit('setLoading', false);
+
+        throw error.response;
+      }
+    },
+  },
+};
