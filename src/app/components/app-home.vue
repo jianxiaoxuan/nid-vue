cb170e4651cb99bffca6fa7c8bab30c4e7aed088
xiaoxuan <xiaoxuan@qq.com>
2021-08-20 02:15:16 +0800

    推送通知提示信息（pushMessage）

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,15 +1,29 @@
 <template>
   <div>欢迎访问宁皓网：） {{ isLoggedIn }}</div>
+  <button class="button" @click="onClickPushMessageButton">Push Message</button>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   computed: {
     ...mapGetters({
       isLoggedIn: 'auth/isLoggedIn',
     }),
+  },
+
+  methods: {
+    ...mapActions({
+      pushMessage: 'notification/pushMessage',
+    }),
+
+    onClickPushMessageButton() {
+      this.pushMessage({
+        content: '空山新雨后，天气晚来秋',
+        icon: 'cloud',
+      });
+    },
   },
 };
 </script>
