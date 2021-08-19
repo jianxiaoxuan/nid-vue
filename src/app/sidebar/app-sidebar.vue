5051712ab47b7dd648e8b0a5abaf56f0f374d53a
xiaoxuan <xiaoxuan@qq.com>
2021-08-19 14:53:53 +0800

    定义主题切换器组件（AppSidebarItemTheme）

--- src/app/sidebar/app-sidebar.vue
+++ src/app/sidebar/app-sidebar.vue
@@ -3,6 +3,7 @@
     <AppSidebarItemBrand />
     <AppSidebarItemPopular />
     <AppSidebarItemAdd />
+    <AppSidebarItemTheme />
   </div>
 </template>
 
@@ -11,6 +12,7 @@ import { defineComponent } from 'vue';
 import AppSidebarItemBrand from './components/app-sidebar-item-brand.vue';
 import AppSidebarItemPopular from './components/app-sidebar-item-popular.vue';
 import AppSidebarItemAdd from './components/app-sidebar-item-add.vue';
+import AppSidebarItemTheme from './components/app-sidebar-item-theme.vue';
 
 export default defineComponent({
   name: 'AppSidebar',
@@ -51,6 +53,7 @@ export default defineComponent({
     AppSidebarItemBrand,
     AppSidebarItemPopular,
     AppSidebarItemAdd,
+    AppSidebarItemTheme,
   },
 });
 </script>
