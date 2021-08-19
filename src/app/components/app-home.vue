6296167da50d1022aa444669026fb9c3b612fb30
xiaoxuan <xiaoxuan@qq.com>
2021-08-19 11:23:39 +0800

    定义应用标志组件（AppLogo）

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,3 +1,14 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
+  <AppLogo size="64" color="#6435c9" />
 </template>
+
+<script>
+import AppLogo from '@/app/components/app-logo.vue';
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
