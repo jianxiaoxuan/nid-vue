e5d8e2b3043f3bbd12c5e0c25fb20dd8e1c547b8
xiaoxuan <xiaoxuan@qq.com>
2021-08-22 17:20:11 +0800

    用户菜单项目

--- src/user/components/user-menu.vue
+++ src/user/components/user-menu.vue
@@ -4,6 +4,22 @@
     <div class="user-menu-header">
       <UserName :user="currentUser" />
     </div>
+    <div
+      class="user-menu-items"
+      v-for="(menuItems, menuItemsIndex) in menu"
+      :key="menuItemsIndex"
+    >
+      <div
+        class="user-menu-item"
+        v-for="(item, index) in menuItems"
+        :key="index"
+        @click="$emit('close')"
+      >
+        <router-link class="link" :to="item.linkTo">
+          {{ item.text }}
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,6 +56,36 @@ export default defineComponent({
     ...mapGetters({
       currentUser: 'user/currentUser',
     }),
+
+    menu() {
+      return [
+        [
+          {
+            linkTo: {
+              name: 'userPosts',
+              params: { userId: this.currentUser.id },
+            },
+            text: '作品',
+          },
+          {
+            linkTo: {
+              name: 'userComments',
+              params: { userId: this.currentUser.id },
+            },
+            text: '评论',
+          },
+        ],
+        [
+          {
+            linkTo: {
+              name: 'userAccount',
+              params: { userId: this.currentUser.id },
+            },
+            text: '账户',
+          },
+        ],
+      ];
+    },
   },
 
   /**
