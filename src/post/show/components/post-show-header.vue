4125dd6fc38e873616467005c5e311a017946f2d
xiaoxuan <xiaoxuan@qq.com>
2021-08-23 17:26:36 +0800

    定义内容页面头部组件

---
+++ src/post/show/components/post-show-header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="post-show-header">
+    <div class="content">
+      <h1 class="text">{{ post.title }}</h1>
+      <div class="meta">
+        <UserName :user="post.user" />
+      </div>
+    </div>
+    <div class="thumbnail">
+      <UserAvatar :user="post.user" size="medium" link="user" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import UserAvatar from '@/user/components/user-avatar.vue';
+import UserName from '@/user/components/user-name.vue';
+
+export default defineComponent({
+  name: 'PostShowHeader',
+
+  /**
+   * 属性
+   */
+  props: {
+    post: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {},
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {
+    UserAvatar,
+    UserName,
+  },
+});
+</script>
+
+<style scoped>
+@import './styles/post-show-header.css';
+</style>
