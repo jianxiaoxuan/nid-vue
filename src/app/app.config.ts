41520347ac8ad171498f71734fc5f3b426d1ebb9
xiaoxuan <xiaoxuan@qq.com>
2021-08-26 18:08:08 +0800

    设置每页评论数量

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,3 +12,11 @@ export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
  * 每页内容数量
  */
 export const POSTS_PER_PAGE = parseInt(process.env.VUE_APP_POSTS_PER_PAGE, 10);
+
+/**
+ * 每页评论数量
+ */
+export const COMMENTS_PER_PAGE = parseInt(
+  process.env.VUE_APP_COMMENTS_PER_PAGE,
+  10,
+);
