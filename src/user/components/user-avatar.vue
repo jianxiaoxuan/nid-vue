f4d0bd9bb20144714043835460e9ca51f04d30f8
xiaoxuan <xiaoxuan@qq.com>
2021-08-26 16:58:30 +0800

    改进：用户头像图像加载

--- src/user/components/user-avatar.vue
+++ src/user/components/user-avatar.vue
@@ -3,7 +3,7 @@
     <router-link class="link" :to="userAvatarLinkTo" v-if="link">
       <img class="image" :src="userAvatarSource" />
     </router-link>
-    <img v-else class="image" :src="userAvatarSource" />
+    <img v-else class="image" :src="userAvatarSource" @load="onLoadImage" />
   </div>
 </template>
 
@@ -37,7 +37,9 @@ export default defineComponent({
    * 数据
    */
   data() {
-    return {};
+    return {
+      loading: true,
+    };
   },
 
   /**
@@ -55,6 +57,7 @@ export default defineComponent({
         'user-avatar',
         this.size,
         { fade: !this.isLoggedIn && !this.user },
+        { loading: this.loading },
       ];
     },
 
@@ -106,7 +109,11 @@ export default defineComponent({
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    onLoadImage() {
+      this.loading = false;
+    },
+  },
 
   /**
    * 使用组件
