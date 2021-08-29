d91f60e3037b10d906a02a950dccb8ebb0902f66
xiaoxuan <xiaoxuan@qq.com>
2021-08-29 20:38:11 +0800

    定义更新评论Store模块

--- src/comment/comment.store.ts
+++ src/comment/comment.store.ts
@@ -8,8 +8,10 @@ import {
 
 import { CommentCreateStoreState, commentCreateStoreModule } from '@/comment/create/comment-create.store';
 import { CommentDestroyStoreState, commentDestroyStoreModule } from '@/comment/destroy/comment-destroy.store';
+import { CommentEditStoreState, commentEditStoreModule } from '@/comment/edit/comment-edit.store';
 
 export interface CommentStoreState {
+  edit: CommentEditStoreState;
   destroy: CommentDestroyStoreState;
   create: CommentCreateStoreState;
   index: CommentIndexStoreState;
@@ -45,6 +47,7 @@ export const commentStoreModule: Module<CommentStoreState, RootState> = {
    * 模块
    */
   modules: {
+    edit: commentEditStoreModule,
     destroy: commentDestroyStoreModule,
     create: commentCreateStoreModule,
     index: commentIndexStoreModule,
