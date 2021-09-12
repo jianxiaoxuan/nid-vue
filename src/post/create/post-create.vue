c6fc04062e3a99846b627a30e659fb91f780d7fc
xiaoxuan <xiaoxuan@qq.com>
2021-09-12 19:34:41 +0800

    上传与创建文件

--- src/post/create/post-create.vue
+++ src/post/create/post-create.vue
@@ -54,6 +54,7 @@ export default defineComponent({
       title: 'post/create/title',
       content: 'post/create/content',
       post: 'post/show/post',
+      selectedFile: 'file/create/selectedFile',
     }),
   },
 
@@ -93,6 +94,7 @@ export default defineComponent({
       getPostById: 'post/show/getPostById',
       updatePost: 'post/edit/updatePost',
       deletePost: 'post/destroy/deletePost',
+      createFile: 'file/create/createFile',
     }),
 
     ...mapMutations({
@@ -112,6 +114,7 @@ export default defineComponent({
             title: this.title,
             content: this.content,
           },
+          file: this.selectedFile,
         });
 
         this.$router.push({
@@ -190,6 +193,20 @@ export default defineComponent({
 
       if (!this.title) {
         this.setTitle(file.name.split('.')[0]);
+      }
+
+      if (this.postId) {
+        this.submitCreateFile();
+      } else {
+        this.submitCreatePost();
+      }
+    },
+
+    async submitCreateFile() {
+      try {
+        await this.createFile({ postId: this.postId, file: this.selectedFile });
+      } catch (error) {
+        this.pushMessage({ content: error.data.message });
       }
     },
   },
